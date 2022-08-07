b24b24b01733485361d124e7f2ff558f445ffb45
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2022-08-07 20:59:16 +0200

    cards added to home, new files added, fade in&out between pages

---
+++ js/index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+function fadeInPage() {
+  if (!window.AnimationEvent) {
+    return;
+  }
+  let fade = document.getElementById("fade");
+  fade.setAttribute("class", "fade-out");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (!window.AnimationEvent) {
+    return;
+  }
+  let anchors = document.getElementsByTagName("a");
+  for (let x = 0; x < anchors.length; x += 1) {
+    if (
+      anchors[x].hostname !== window.location.hostname ||
+      anchors[x].pahtname !== window.location.pathname
+    ) {
+      continue;
+    }
+    anchors[x].addEventListener("click", function (e) {
+      let fade = document.getElementById("fade"),
+        anchor = e.currentTarget;
+      let listener = function () {
+        window.location = anchor.href;
+        fade.removeEventListener("animationend", listener);
+      };
+      fade.addEventListener("animationend", listener);
+      event.preventDefault();
+      fade.setAttribute("class", "fade-in");
+    });
+  }
+});
+
+window.addEventListener("pageshow", function (e) {
+  if (!event.persisted) {
+    return;
+  }
+  let fade = document.getElementById("fade");
+  fade.removeAttribute("class", "fade-in");
+});
