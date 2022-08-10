eb114d28606c69f956197e1fa05763933d9f4873
MikolajBuelens <mikolaj.buelens@student.ehb.be>
2022-08-10 21:49:02 +0200

    removed clutter (comments, unnecessary code), better div names, reduced css files

---
+++ js/changeText.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function changeIntro(size) {
+    let introText = document.getElementById("intro");
+    if (size.matches) {
+      console.log("match");
+      introText.innerHTML = "<span>Hallo world,<br>I'm Mikolaj<span>";
+    }  else {
+      console.log("not a match");
+      
+      introText.innerHTML =" console.log(<span>'Hallo world, I'm Mikolaj!'</span>);";
+      //   "console.log(<span>'Hallo world, I'm Mikolaj!'</span>);";
+    }
+  }
+  let size = window.matchMedia("(max-width:685px)");
+  changeIntro(size);
+  size.addListener(changeIntro);
--- js/index.js
+++ js/index.js
@@ -25,20 +25,6 @@ function toggle() {
 // displayNav(size);
 // size.addListener(displayNav);
 
-function changeIntro(size) {
-  let introText = document.getElementById("intro");
-  if (size.matches) {
-    console.log("match");
-    introText.innerHTML = "<span>Hallo world,<br>I'm Mikolaj<span>";
-  }  else {
-    console.log("not a match");
-    
-    introText.innerHTML ="  console.log(<span>'Hallo world, I'm Mikolaj!'</span>);";
-    //   "console.log(<span>'Hallo world, I'm Mikolaj!'</span>);";
-  }
-}
-let size = window.matchMedia("(max-width:565px)");
-changeIntro(size);
-size.addListener(changeIntro);
+
 
 
